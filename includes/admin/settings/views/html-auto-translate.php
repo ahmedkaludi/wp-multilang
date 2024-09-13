c6085631da255c30698cf1607fb617932f99a3d1
shridhamdeveloper <shridham55@gmail.com>
2024-09-13 17:03:18 +0530

    Auto translation feature changes

---
+++ includes/admin/settings/views/html-auto-translate.php
@@ -0,0 +1,5 @@
+<p id="wpm-auto-translation-free-note"> <?php esc_html_e( 'This Feature requires the Premium Version', 'wp-multilang' ); ?> 
+	<a href="https://wp-multilang.com/pricing/#pricings" target="_blank" class="wpm-premium-btn">
+		<span><?php esc_html_e('Upgrade Now', 'wp-multilang'); ?><span>
+	</a>
+</p>
