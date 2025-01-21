fc8184c4c96f5614a5a42016d41ed7b702a6fca2
shridhamdeveloper <38998323+shridhamdeveloper@users.noreply.github.com>
2025-01-21 17:38:16 +0530

    #138

--- assets/scripts/wpm-feedback-script.js
+++ assets/scripts/wpm-feedback-script.js
@@ -10,16 +10,10 @@ jQuery(document).ready(function ($) {
 
     wpm_deactivate_link.click(function (e) {
         e.preventDefault();
-        // only show feedback form once per 30 days
-        var c_value = wpm_admin_get_cookie("wpm_hide_deactivate_feedback");
+        $('#wpm-reloaded-feedback-overlay').show();
 
-        if (c_value === undefined) {
-            $('#wpm-reloaded-feedback-overlay').show();
-        } else {
-            // click on the link
-            window.location.href = wpm_deactivate_link_url;
-        }
     });
+    
     // show text fields
     $('#wpm-reloaded-feedback-content input[type="radio"]').click(function () {
         // show text field if there is one
@@ -28,13 +22,10 @@ jQuery(document).ready(function ($) {
         $(".mb-box").not(targetBox).hide();
         $(targetBox).show();
     });
+    
     // send form or close it
     $('#wpm-reloaded-feedback-content .button').click(function (e) {
         e.preventDefault();
-        // set cookie for 30 days
-        var exdate = new Date();
-        exdate.setSeconds(exdate.getSeconds() + 2592000);
-        document.cookie = "wpm_hide_deactivate_feedback=1; expires=" + exdate.toUTCString() + "; path=/";
 
         $('#wpm-reloaded-feedback-overlay').hide();
         if ('wpm-reloaded-feedback-submit' === this.id) {
@@ -44,7 +35,7 @@ jQuery(document).ready(function ($) {
                 url: ajaxurl,
                 dataType: 'json',
                 data: {
-                    action: 'wpm_send_feedback',
+                    action: 'wpm_deactivate_plugin',
                     data: $('#wpm-reloaded-feedback-content form').serialize()
                 },
                 complete: function (MLHttpRequest, textStatus, errorThrown) {
@@ -59,23 +50,10 @@ jQuery(document).ready(function ($) {
             window.location.href = wpm_deactivate_link_url;
         }
     });
+
     // close form without doing anything
     $('.wpm-feedback-not-deactivate').click(function (e) {
         $('#wpm-reloaded-feedback-overlay').hide();
     });
-    
-    function wpm_admin_get_cookie (name) {
-    var i, x, y, wpm_cookies = document.cookie.split( ";" );
-    for (i = 0; i < wpm_cookies.length; i++)
-    {
-        x = wpm_cookies[i].substr( 0, wpm_cookies[i].indexOf( "=" ) );
-        y = wpm_cookies[i].substr( wpm_cookies[i].indexOf( "=" ) + 1 );
-        x = x.replace( /^\s+|\s+$/g, "" );
-        if (x === name)
-        {
-            return unescape( y );
-        }
-    }
-}
 
 }); // document ready
--- includes/admin/tracking/class-wpm-feedback-form.php
+++ includes/admin/tracking/class-wpm-feedback-form.php
@@ -40,47 +40,39 @@ class WPM_Deactivate_Feedback_Form{
      * */
     public function wpm_add_deactivation_feedback_modal()
     {
-        $current_user = wp_get_current_user();
-        $email = '';
-        if(is_object($current_user) && isset($current_user->user_email)){
-            $email = trim( $current_user->user_email );
-        }
-
-        $reasons = array(
-            1 => '<li><label><input type="radio" name="wpm_disable_reason" value="temporary"/>' . esc_html__('It is only temporary', 'wp-multilang') . '</label></li>',
-            2 => '<li><label><input type="radio" name="wpm_disable_reason" value="stopped"/>' . esc_html__('I stopped using WP Multilang plugin on my site', 'wp-multilang') . '</label></li>',
-            3 => '<li><label><input type="radio" name="wpm_disable_reason" value="missing"/>' . esc_html__('I miss a feature', 'wp-multilang') . '</label></li>
-            <li><input class="mb-box missing" type="text" name="wpm_disable_text[]" value="" placeholder="' .esc_attr__( 'Please describe the feature', 'wp-multilang') . '"/></li>',
-            4 => '<li><label><input type="radio" name="wpm_disable_reason" value="technical"/>' . esc_html__('Technical Issue', 'wp-multilang') . '</label></li>
-            <li><textarea class="mb-box technical" name="wpm_disable_text[]" placeholder="' . esc_attr__('How Can we help? Please describe your problem', 'wp-multilang') . '"></textarea></li>',
-            5 => '<li><label><input type="radio" name="wpm_disable_reason" value="another plugin"/>' . esc_html__('I switched to another plugin', 'wp-multilang') .  '</label></li>
-            <li><input class="mb-box another" type="text" name="wpm_disable_text[]" value="" placeholder="' .esc_attr__( 'Name of the plugin', 'wp-multilang' ) . '"/></li>',
-            6 => '<li><label><input type="radio" name="wpm_disable_reason" value="other"/>' . esc_html__('Other reason', 'wp-multilang') . '</label></li>
-            <li><textarea class="mb-box other" name="wpm_disable_text[]" placeholder="' . esc_attr__('Please specify, if possible', 'wp-multilang') . '"></textarea></li>',
-        );
-        shuffle($reasons);
-
+        $current_user   =   wp_get_current_user();
+        $uninstall_opt  =   get_option( 'wpm_uninstall_translations', 'no' );
         ?>
 
         <div id="wpm-reloaded-feedback-overlay" style="display: none;">
             <div id="wpm-reloaded-feedback-content">
             <form action="" method="post">
-                <h3><strong><?php esc_html_e('If you have a moment, please let us know why you are deactivating:', 'wp-multilang'); ?></strong></h3>
-                <ul>
-                        <?php 
-                        foreach ($reasons as $reason_escaped){
-                            //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- all html inside this variable already escaped above in $reasons variable
-                            echo $reason_escaped;
-                        }
-                        ?>
-                </ul>
-                <?php if ($email) : ?>
-                    <input type="hidden" name="wpm_disable_from" value="<?php echo esc_attr($email); ?>"/>
-                <?php endif; ?>
+                <p><strong><?php echo esc_html__( 'Note: ', 'wp-multilang' ); ?></strong><?php echo esc_html__( 'Because the plugin stores data in different ways, WP Multilang may display unnecessary data if you uninstall and delete it without enabling the "Delete Translation" option. If it is a temporary deactivation, you can proceed with deactivation only', 'wp-multilang' ) ?></p>
+                <p><?php echo esc_html__( 'Below are a few document links that will guide you on how to clean up data during a permanent uninstallation and deletion of WP Multilang plugin', 'wp-multilang' ); ?></p>
+                <ol>
+                    <li>
+                        <strong><a href="https://wp-multilang.com/docs/knowledge-base/how-to-uninstall-wp-multilang-plugin-properly/" target="_blank"><?php echo esc_html__( 'How to uninstall WP-Multilang plugin properly?', 'wp-multilang' ); ?></a></strong>
+                    </li>
+                    <li>
+                        <strong><a href="https://wp-multilang.com/docs/knowledge-base/my-website-broke-after-deactivating-wp-multilang-how-to-fix-it/" target="_blank"><?php echo esc_html__( 'My website broke after deactivate, how to fix it?', 'wp-multilang' ); ?></a></strong>
+                    </li>
+                </ol>
+
+                <table class="form-table">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input name="wpm_uninstall_translations" id="wpm_uninstall_translations" type="checkbox" value="1" <?php checked( $uninstall_opt, 'yes' ); ?> />
+                                <label for="wpm_uninstall_translations" class="wpm-cursor-pointer"><strong><?php echo esc_html__( 'Delete Translations', 'wp-multilang' ); ?></strong> <?php echo esc_html__( ' - Enabling this option will delete all the translation.', 'wp-multilang'); ?></label>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
                 <input id="wpm-reloaded-feedback-submit" class="button button-primary" type="submit" name="wpm_disable_submit" value="<?php esc_html_e('Submit & Deactivate', 'wp-multilang'); ?>"/>
                 <a class="button"><?php esc_html_e('Only Deactivate', 'wp-multilang'); ?></a>
                 <a class="wpm-feedback-not-deactivate" href="#"><?php esc_html_e('Don\'t deactivate', 'wp-multilang'); ?></a>
-                <?php wp_nonce_field( 'wpm_feedback_nonce', 'wpm_feedback_nonce' );   ?>
+                <?php wp_nonce_field( 'wpm_deactivate_plugin_nonce', 'wpm_deactivate_plugin_nonce' );   ?>
             </form>
             </div>
         </div>
